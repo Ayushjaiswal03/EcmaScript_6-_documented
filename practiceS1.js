43a1c7b03a2e738c75e5d2e8f5a1141d5d7306cc
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-09 02:38:31 +0530

    P 5

--- practiceS1.js
+++ practiceS1.js
@@ -8,8 +8,8 @@
     // console.log(typeof a + b) //No
 
     let c = 31;
-    console.log(a + c)
-    console.log(typeof (a + c))
+    // console.log(a + c)
+    // console.log(typeof (a + c))
 
 
     const PS1 = () => {
@@ -21,8 +21,18 @@
 
         d.num = 4;
         // console.log(d);
-        return d
-        }
-        console.log(PS1())
+        return d;
+    }
+    console.log(PS1());
+
+    const dict = {
+        earphones : "device you put on your ears for better listening",
+        lighter : "for instant flame and to light a flame as well",
+        charger : `to charge every device just like`,
+        watch : "it tells you time",
+        book : "to read/ write text"
+    }
+
+    console.log(dict.book);
 
 }
