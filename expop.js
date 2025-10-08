d45368d1633abb0e8e06bfbd0fe4660a0049a315
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-09 03:22:59 +0530

    exp and operators

---
+++ expop.js
@@ -0,0 +1,50 @@
+console.log("operators in JS");
+
+//
+
+let a = 10;
+let b = 4;
+// console.log(a + b);
+// console.log(a / b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b)
+// console.log(a ** b);
+console.log("incrementing a = ", ++a )
+console.log(a++)
+console.log(a--)
+console.log(--a)
+
+//Assignement operators
+let ass = 8;
+ass += 5;
+ass -= 6
+ass *= 3
+ass /= 3
+console.log(ass);
+
+//comparison operators
+console.log("comaprison operator")
+let nums1 = 30;
+let nums2 = "30";
+
+console.log("nums1 == nums2", nums1 == nums2)
+console.log("nums1 != nums2", nums1 != nums2)
+console.log("nums1 === nums2", nums1 === nums2)
+console.log("nums1 !== nums2", nums1 !== nums2)
+
+//logical operators
+console.log("Logical Operators");
+let x = 5;
+let y = 6;
+
+console.log(x<y && x==5)
+console.log(x<y || x==y)
+console.log(!true)
+
+
+
+
+
+
+
