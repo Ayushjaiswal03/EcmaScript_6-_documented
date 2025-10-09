2d4b490a940ef83a03f89efdec83e36e582f6466
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-09 16:23:40 +0530

    ifswitch

--- conditionalexp.js
+++ conditionalexp.js
@@ -2,17 +2,46 @@ console.log("CE");
 
 // let a = Number.parseInt(prompt("Hey, what's your age?"));
 
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-rl.question("Hey, what's your age? ", (answer) => {
-    console.log(typeof answer)
-  let age = Number.parseInt(answer);
-  console.log(`You entered: ${age}`);
-  rl.close();
-});
+// rl.question("Hey, what's your age? ", (answer) => {
+//     console.log(typeof answer)
+//   let age = Number.parseInt(answer);
+//   console.log(`You entered: ${age}`);
+//   rl.close();
+// });
 
+
+//if else....else if
+let agge = 18.6554;
+let age = Math.ceil(agge);
+
+if (age < 18) {
+    console.log("minor");
+     age;
+} else if(age > 18) {
+    console.log("too old");
+     console.log (age);
+}else {
+    console.log("perfect 18")
+}
+
+//switch
+switch(age && agge) {
+    case 1:
+        console.log("minor");
+    break;
+    case 2: 
+    console.log("major");
+    break;
+    default: 
+    console.log("perfect 18")
+}
+
+
+//  console,log()
