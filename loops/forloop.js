368004a9809bfdd8e9f3b8b08e29e59c1a44ecdf
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-09 23:31:38 +0530

    more on loops

---
+++ loops/forloop.js
@@ -0,0 +1,22 @@
+console.log("hello loops")
+
+// for (let i = 0; i < 30 ; i++){
+//     console.log(i);
+// }
+
+//write a sample code for for loop
+// let n = Math.ceil(Math.random() * 10);
+// console.log(`n is ${n}`)
+// for (let i = 0; i < n; i++) {
+//     console.log(i)
+    
+// }
+
+let arr = [1, 2, 3, 4, 5];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+
+}
+
+
