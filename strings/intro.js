ade51e23cc79d5c43113acc39804b8c1af8118ef
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-10 15:17:48 +0530

    str interpolation and escape char

---
+++ strings/intro.js
@@ -0,0 +1,11 @@
+console.log("strings");
+
+// let name = "Ayush"
+// console.log(name[1])
+
+let boy1 = "Ayush";
+let boy2 = "Jaiswal";
+
+console.log(`${boy1} of house ${boy2}`);
+
+console.log("this is the house which belongs to Ayush\"s")
