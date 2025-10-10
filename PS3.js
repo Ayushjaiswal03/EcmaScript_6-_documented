2826eaba9ab038e7b31ee2ac0cbc9d6648e76bce
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-10 13:36:43 +0530

    PS3

--- PS3.js
+++ PS3.js
@@ -11,6 +11,27 @@ let obj = {
 //     console.log(Object.keys(obj)[i] + " has gottten the marks of " + obj[Object.keys(obj)[i]]);
 // }
 
-for (let marks in obj){
-    console.log(marks + " has scored " + obj[marks]);
-}
+// for (let marks in obj){
+//     console.log(marks + " has scored " + obj[marks]);
+// }
+
+//
+const readline = require("readline");
+const rl = readline.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+rl.question("write the number which is divisible by 10 ", (answer) => {
+    console.log(typeof answer);
+    let n = Number.parseInt(answer);
+    
+    if(n % 10 == 0){
+        console.log("number is. divisble by 10")
+    }else {
+        console.log("try again");
+    }
+
+    rl.close();
+
+})
