09546f39f923d3e10e0589e1d773792d523b9742
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-11 19:35:57 +0530

    subtag.html ccss

---
+++ PracticeS4.js
@@ -0,0 +1,23 @@
+//
+console.log("har\"".length);
+// //willl give 4 coz \ this is not counted
+
+// let str = "Hi this is ";
+// console.log(str.includes("this"))
+// //gives true if the string or the part of it exists
+
+// console.log(str.startsWith("Hi"))
+// str.trimEnd(" ")
+// console.log(str.endsWith(""))
+// let str2 = str.toUpperCase()
+// console.log(str2);
+// console.log(str2.toLowerCase())
+// //
+
+let amount = "Please give RS 100";
+console.log(amount.slice(15, 18));
+
+//
+let sent = "Were you able to do it?";
+console.log(sent.replace("to", "\,so"))
+
