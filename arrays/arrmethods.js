53b4ee947f6b7a15f538e0705878096df7c92f3b
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-11 20:42:02 +0530

    flat

--- arrays/arrmethods.js
+++ arrays/arrmethods.js
@@ -1,17 +1,25 @@
 console.log("methods");
 
 let num = [1, 12, 15, 39, 54];
-let b = num.toString();
-let c = num.join(" and ");
-console.log(c);
-//updating the same and not creating the new one
-let r = num.pop();
-//r for popped element
-num.push("Apple", 21)
+// let b = num.toString();
+// let c = num.join(" and ");
+// console.log(c);
+// //updating the same and not creating the new one
+// let r = num.pop();
+// //r for popped element
+// num.push("Apple", 21)
 
-num.unshift("guava", 69, 91);
-let t = num.shift()
-let  y=  delete num[4];
-console.log(num, r, t, y);
+// num.unshift("guava", 69, 91);
+// let t = num.shift()
+// let  y=  delete num[4];
+// console.log(num, r, t, y);
+
+let arr = [1, 2, 3, 4, 5];
+num[3] = arr;
+arr.push([1, 0, 2, 9, 3, 8, 4])
+
+let arr2 = num.flat(2)
+console.log(arr2)
+
 
 
