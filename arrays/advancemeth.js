a77e82d12ba2ac25cdcc8a3a958b0407128a146e
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-11 21:55:37 +0530

    array search methods

---
+++ arrays/advancemeth.js
@@ -0,0 +1,12 @@
+console.log("advance methods");
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+// delete arr[6];
+// console.log(arr);
+
+// let arr2 = [0, 9, 8, 7, 6]
+
+// let arr3 = arr.concat(arr2)
+console.log(arr.lastIndexOf(4, 6)); //IndexOf(), aage se search krta hai, lastIndexOf() peeche se
+console.log(arr.find(num => num > 3));  //includes() checks and gives true or false depending on the presence of element, 
+
