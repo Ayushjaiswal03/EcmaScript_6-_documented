210f3d177bdfd9e2023be1aa013725dbc31a883b
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-11 20:50:28 +0530

    last basics of the methods

--- arrays/arrmethods.js
+++ arrays/arrmethods.js
@@ -15,11 +15,17 @@ let num = [1, 12, 15, 39, 54];
 // console.log(num, r, t, y);
 
 let arr = [1, 2, 3, 4, 5];
-num[3] = arr;
-arr.push([1, 0, 2, 9, 3, 8, 4])
+// num[3] = arr;
+// arr.push([1, 0, 2, 9, 3, 8, 4])
 
-let arr2 = num.flat(2)
-console.log(arr2)
+// let arr2 = num.flat(2)
+let left = arr.splice(1, 2)
+console.log(arr, left)
+let a = num.splice(1, 3, 34);
+console.log(num, a);
+let arr3 = num.concat(arr);
+// let arr4 = num.join(arr)
+console.log(arr3)
 
 
 
