509fc07ff2f0ba7b08ea8d3f726c0a404ffcc0d9
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-11 08:12:13 +0530

    str iteration

---
+++ strings/strmethods.js
@@ -0,0 +1,35 @@
+console.log("str methods")
+
+// let name = "Ayush";
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4))
+// console.log(name.replace("Ayush", "swapnil"))
+// console.log(name.charAt(2))
+// console.log(name.charCodeAt(2))
+// console.log(name.concat(" Jaiswal"))
+// console.log(name.)
+
+// let fr ="       shivika     ";
+// console.log(fr[0])
+// console.log(fr[1])
+// console.log(fr[2])
+// console.log(fr[3])
+// console.log(fr[5])
+// console.log(fr[6])
+
+// console.log(fr.trim())
+
+let str = "hello";
+let set = new Set(str)
+console.log(set)
+
+// for(let i in str){
+//     console.log(str.slice(str[str.length - 1]))
+// }
+let arr = []
+for(let i = 0; i < str.length; i++){
+    
+    arr.push(str[i])
+    console.log(arr)
+}
