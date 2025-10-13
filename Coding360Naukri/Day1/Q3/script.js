07115c0c97a962ea4200573d6e3521d96ea073e9
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-14 04:42:30 +0530

    CSS completed for Q2

---
+++ Coding360Naukri/Day1/Q3/script.js
@@ -0,0 +1,8 @@
+window.userScore = 0;
+window.computerScore = 0;
+
+function Computer () {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    
+}
