16da6fd1a6db0572cce9f675993f11c7c05c83c1
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-13 22:59:25 +0530

    functions added to reject negtive or NaN input

--- PracticeS6.js
+++ PracticeS6.js
@@ -1,15 +1,25 @@
 console.log("This is the Practice Set")
 
-//Q1
+//Q1 && Q2
 const readline = require("readline");
 const rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout
 })
+const askAge = () =>{
 rl.question("tell me your age here ", (answer) => {
-    // console.log(answer);
     let num = Number.parseInt(answer);
 
+    if(isNaN(num)){
+        console.error("Write a valid number")
+        return askAge();
+    }
+
+    if(num < 0){
+        console.error(`${num} cannot be negative`)
+        return askAge();
+    }
+    
     if (num >= 18){
         console.log("You can drive mf, drink n drive")
     }else if (num == 18){
@@ -18,3 +28,4 @@ rl.question("tell me your age here ", (answer) => {
         console.log("chutiye license laa")
     }
 })
+}
