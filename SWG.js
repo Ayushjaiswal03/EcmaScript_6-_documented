ab81a3fec5f8f63aa670e85b88be8c9b168d8b47
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-15 04:40:02 +0530

    Complete SWG game

--- SWG.js
+++ SWG.js
@@ -1,35 +1,53 @@
-const readline = require("readline");
+const game = () => {
+    const readline = require("readline");
 const rl = readline.createInterface({
     input : process.stdin,
     output: process.stdout
 })
 
-rl.question("Let's play Snake, Water and Gun! you choose = ", (response) => {
-    let a = response.trim();
-    
-    const arr = ["S", "W", "G"];
-    const randomIndex = Math.floor(Math.random() * arr.length);
+const play = () => {
+    rl.question("Let's play Snake, Water and Gun! you choose = ", (response) => {
+        let a = response.trim();
+        
+        const arr = ["S", "W", "G"];
+        const randomIndex = Math.floor(Math.random() * arr.length);
 
-    const compChoice = arr[randomIndex];
+        const compChoice = arr[randomIndex];
 
-    if (a == compChoice) {
-        return console.log("the game is tied");
-    }
+        if (a == compChoice) {
+            return console.log("the game is tied");
+        }
 
-    if((a == "S" && compChoice == "W") ||
-        (a == "W" && compChoice == "G") ||
-        (a == "G" && compChoice == "S")
-){
-    return console.log("You won, handsome!")
-}else {
-    return console.log("MAC bro won!")
+        if((a == "S" && compChoice == "W") ||
+            (a == "W" && compChoice == "G") ||
+            (a == "G" && compChoice == "S")
+        ){
+            return console.log("You won, handsome!")
+        }else {
+            return console.log("MAC bro won!")
+        }
+        // else if(a == "S" && compChoice == "G"){
+        //     console.log("Mac won");
+        // } else if(a == "S" && compChoice == "W"){
+        //     console.log("you won")
+        // }else if(a == "W" && compChoice == "S"){
+        //     console.log("")
+        // }
+    })
 }
-    
-    // else if(a == "S" && compChoice == "G"){
-    //     console.log("Mac won");
-    // } else if(a == "S" && compChoice == "W"){
-    //     console.log("you won")
-    // }else if(a == "W" && compChoice == "S"){
-    //     console.log("")
-    // }
+
+rl.question("Hey handsome, wanna play again? (Y/N) ", (answer) => {
+    const y = answer.toLowerCase();
+    if(y == 'y' || y == 'yes'){
+        return play();
+    }else{
+        return console.log("Tata bye bye, take care")
+        rl.close();
+    }
+            
 })
+
+
+}
+
+game();
