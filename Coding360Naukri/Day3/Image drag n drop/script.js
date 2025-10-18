4944574dce83c5d688569fca70dcb2a70393d15a
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-18 20:00:21 +0530

    function to load the image in js

---
+++ Coding360Naukri/Day3/Image drag n drop/script.js
@@ -0,0 +1,11 @@
+const images = [
+    {
+        src: 'https://images.unsplash.com/photo-1526034332220-067b0f400e06?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGlnZXJ8ZW58MHx8MHx8fDA%3D',
+        name: 'tiger'
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1516642499105-492ff3ac521b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+        name: 'lion'
+    }
+];
+
