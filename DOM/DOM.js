d6f924dd402d59a3e3008593b23b41a8d5397f3a
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-19 04:01:59 +0530

    accesing the parent node , element next sibling and document.body.

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,7 +1,13 @@
-console.log("DOM intro")
-console.log(document.body.firstChild)
-console.log(document.body.lastChild)
-let arr = Array.from(document.body.childNodes)
-console.log(arr)
+// console.log("DOM intro")
+// console.log(document.body.firstChild)
+// console.log(document.body.lastChild)
+// let arr = Array.from(document.body.childNodes)
+// console.log(arr)
 
+console.log(document.body.firstChild)
+let a = document.body.firstChild;
+
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
 
