4c36bcdd9414c0f37872ec35c766c700368a1262
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-19 10:12:02 +0530

    using parent and child elements for nav

--- DOM/elementsonly/elementonly.js
+++ DOM/elementsonly/elementonly.js
@@ -1,1 +1,21 @@
+const changeColor = () => {
+    document.body.firstElementChild.style.background = "red";
+}
+
 let b = document.body;
+
+console.log("First child of b is : ", b.firstChild)
+console.log("first element of b is : ", b.firstElementChild)
+
+//so when we use firstChild , we get text is there is a space between body and the next tag like 
+//<body>    < -----  this space
+// -- > <nav>
+
+// but in firstElementChild, we get the next element , no matter how many spaces we have
+
+console.log(b.previousElementSibling);
+//previous. sibling which is an element
+
+console.log(b.nextElementSibling)
+console.log(b.lastElementChild)
+console.log()
