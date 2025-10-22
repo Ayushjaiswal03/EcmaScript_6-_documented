7962258540ebdba2295b5aec829a0282018b1378
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-22 23:41:02 +0530

    Closest

--- MC&closest/script.js
+++ MC&closest/script.js
@@ -4,3 +4,5 @@ console.log(id1.matches(".class"));
 console.log(id1.matches(".box"));
 // To check if element matches the given CSS selector
 
+console.log(sp1.closest(".box"))
+console.log(sp1.closest("#sp1"))
