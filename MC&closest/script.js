bd74c17d528a350f2e7d88b4324b84dde937f053
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-22 23:41:57 +0530

    contains

--- MC&closest/script.js
+++ MC&closest/script.js
@@ -6,3 +6,8 @@ console.log(id1.matches(".box"));
 
 console.log(sp1.closest(".box"))
 console.log(sp1.closest("#sp1"))
+//To look for the nearest ancestor that matches the given CSS selector. 
+//the element itself is also checked
+
+console.log(id1.contains(sp1));
+console.log(id1.contains(sp1));
