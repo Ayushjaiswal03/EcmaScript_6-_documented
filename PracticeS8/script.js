0a9cfa0803c2a9cc0384a8fd6453277acfe79ac6
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-25 21:32:04 +0530

    SetInterval

---
+++ PracticeS8/script.js
@@ -0,0 +1,9 @@
+document.getElementById('google').addEventListener('click', function() {
+    let url = "https://www.google.com";
+    window.focus();
+})
+
+setInterval(async function () {
+    let url = "https://jsonplaceholder.typicode.com/todos/1"
+    console.log(await fetchContent(url))
+}, 3000)
