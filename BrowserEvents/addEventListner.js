5c5165a7dc66ea405cbd056ac8db4b48dabb7415
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-25 18:04:40 +0530

    remove event listener

--- BrowserEvents/addEventListner.js
+++ BrowserEvents/addEventListner.js
@@ -1,11 +1,35 @@
 let btn = document.getElementById('btn');
 
-btn.addEventListener('click', function(e) {
+// btn.addEventListener('click', function(e) {
+//     // var btn = e.target
+//     alert("Hello world!")
+// } )
+
+// btn.addEventListener('click', function(e) {
+//     // var btn = e.target
+//     alert("Hello world22!")
+// } )
+
+
+let x = function(e) {
     // var btn = e.target
     alert("Hello world!")
-} )
+};
 
-btn.addEventListener('click', function(e) {
+let y = function(e) {
     // var btn = e.target
     alert("Hello world22!")
-} )
+}
+
+btn.addEventListener('click', x);
+
+btn.addEventListener('click', y);
+
+let a = prompt("What is your favourite color");
+
+if (a == "2") {
+    btn.removeEventListener('click', x)
+}
+
+
+// add event listener is responisble to add event or handlers when an event takes placa and remove event listner removes it
