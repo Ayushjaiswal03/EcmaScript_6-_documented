22546ce026053b95824f33944dca5d5400f0a079
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-25 17:45:52 +0530

    adding event listener

---
+++ BrowserEvents/addEventListner.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(e) {
+    // var btn = e.target
+    alert("Hello world!")
+} )
+
+btn.addEventListener('click', function(e) {
+    // var btn = e.target
+    alert("Hello world22!")
+} )
