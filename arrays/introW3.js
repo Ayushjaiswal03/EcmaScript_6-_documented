53acd54636aa9ea20d6f2eef184cecee04c8d894
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-27 12:11:54 +0530

    basics go through

---
+++ arrays/introW3.js
@@ -0,0 +1,41 @@
+console.log("arrays");
+
+
+//How to create an array
+
+// 1 - ---------// const array_name = [item1, item2, item3];
+
+const cars = ["Saab", "Volvo", "BMW"];  //common practice to declare arrays with const keyword
+
+// creating empty array and providing elements or item or values later
+
+// const bikes = [];
+// console.log(bikes);
+
+// bikes[0] = "triumph";
+
+// bikes[1] = "BMW";
+
+// bikes[3] = "KTM";
+// console.log(bikes);
+
+
+// 2 -------------
+
+let arr1 = new Array("Saab", "Volvo", "BMW");
+
+// console.log(arr1);
+
+
+
+//Accessing array Elements
+
+const cars1 = ["Saab", "Volvo", "BMW"];
+
+let car = cars1[0];
+// console.log(car);
+
+// you change the value as well in a particular index
+
+//Arrays are special type of objects
+
