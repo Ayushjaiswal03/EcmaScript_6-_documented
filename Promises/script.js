4d8719ded57163755e64755153a5bb0669192211
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-30 16:57:44 +0530

    resolved promise

--- Promises/script.js
+++ Promises/script.js
@@ -1,14 +1,25 @@
 //the solution to callback hell is promises
-console.log("Hello one");
+// console.log("Hello one");
 
-let promise = new Promise((resolve, reject) => {
-    console.log("hi")
-    resolve(56)
-})
-console.log(promise);
+// let promise = new Promise((resolve, reject) => {
+//     console.log("hi")
+//     resolve(56)
+// })
+// console.log(promise);
 
 // setTimeout(function () {
 //     console.log("Hello two after 2 sec")
 // }, 2000);
 
-console.log("Hello three")
+console.log("Hello three");
+
+let p = new Promise((resolve, reject) => {
+    console.log('promise is pending')
+    setTimeout(() => {
+        console.log('I am a promise and I am fullfilled');
+        // resolve(true)
+        reject(new Error('I am an error'))
+    }, 5000)
+})
+
+console.log(p);
