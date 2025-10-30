8d8de413efd8f3eb0d1786e61649b5f6d5b02e21
Ayushjaiswal03 <ayush60000@gmail.com>
2025-10-30 17:11:00 +0530

    .then() and .catch() and reject

--- Promises/script.js
+++ Promises/script.js
@@ -13,7 +13,18 @@
 
 console.log("Hello three");
 
-let p = new Promise((resolve, reject) => {
+let p1 = new Promise((resolve, reject) => {
+    console.log('promise is pending')
+    setTimeout(() => {
+        console.log('I am a promise and I am fullfilled');
+        resolve(true)
+        // reject(new Error('I am an error'))
+    }, 5000)
+})
+
+console.log(p1);
+
+let p2 = new Promise((resolve, reject) => {
     console.log('promise is pending')
     setTimeout(() => {
         console.log('I am a promise and I am fullfilled');
@@ -22,4 +33,12 @@ let p = new Promise((resolve, reject) => {
     }, 5000)
 })
 
-console.log(p);
+console.log(p2);
+
+p1.then((value) => {
+    console.log(value)
+})
+
+p2.catch((error) => {
+    console.log(`some error occured in p2 which was ${error}`)
+})
