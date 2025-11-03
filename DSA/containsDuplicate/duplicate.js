98d3a756e51ea1d72c6e1e804e591fef76c2dfb6
Ayushjaiswal03 <ayush60000@gmail.com>
2025-11-03 23:53:26 +0530

    brute force to solve duplicate problem

--- DSA/containsDuplicate/duplicate.js
+++ DSA/containsDuplicate/duplicate.js
@@ -27,3 +27,21 @@
 
 // Output: true
 
+
+
+//                                   Brute force: compare every pair → O(n²). Simple but slow.
+
+// for i in 0 - arr.length - 1
+// for j in i+1 - arr.length - 1
+// if arr[i] == arr[j] return true
+
+function containsDuplicate(nums) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j]) return true;
+        }
+    }
+    return false
+}
+console.log(containsDuplicate([1,2, 3, 4, 5, 1]))
+//
