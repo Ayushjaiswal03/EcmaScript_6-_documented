e1e2a01303e5748f7c806b9730a5e1e0621111ec
Ayushjaiswal03 <ayush60000@gmail.com>
2025-11-11 23:16:56 +0530

    js

---
+++ Coding360Naukri/Day20/AgeCalc2.0/script.js
@@ -0,0 +1,20 @@
+document.getElementById("btn").addEventListener('click', function () {
+    const birthDate = document.getElementById("birthday").value;
+    const result = document.getElementById("result");
+
+    if(!birthDate) {
+        result.innerText = "Please select your date of birth!";
+        return;
+    }
+
+    const today = new Date();
+    const dob = new Date(birthDate);
+
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    const dayDiff = today.getDate() - dob.getDate();
+
+    if(monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age --;
+    }
+})
